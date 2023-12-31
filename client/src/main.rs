3511a0e328161faca6a27eff81466b4d1c5cc1d4
cdknight <me@ramesh.tv>
2023-12-31 17:02:44 -0500

    Client converts YUV to RGB

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,11 +1,12 @@
-use std::{net::UdpSocket, time::Instant};
+use std::{net::UdpSocket, process::exit, time::Instant};
 
 use bytes::{BufMut, Bytes, BytesMut};
+use dcv_color_primitives::{convert_image, get_buffers_size, ColorSpace, ImageFormat};
 use flexi_logger::Logger;
 use log::{debug, error, info, warn};
 use openh264::{
     decoder::{DecodedYUV, Decoder, DecoderConfig},
-    formats::YUVBuffer,
+    formats::YUVSource,
 };
 use rtp::{codecs::h264::H264Packet, packet::Packet, packetizer::Depacketizer};
 use webrtc_util::Unmarshal;
@@ -23,7 +24,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut decoder = Decoder::with_config(DecoderConfig::new().debug(true))?;
             let mut buffer = BytesMut::new();
             let mut h264_data: Option<DecodedYUV> = None;
+            let mut rgba_buffer: Option<Vec<u8>> = None;
 
+            let mut width: u32 = 0;
+            let mut height: u32 = 0;
+
+            let src_format = ImageFormat {
+                pixel_format: dcv_color_primitives::PixelFormat::I420,
+                color_space: ColorSpace::Bt601FR,
+                num_planes: 3,
+            };
+            let dst_format = ImageFormat {
+                pixel_format: dcv_color_primitives::PixelFormat::Rgba,
+                color_space: ColorSpace::Rgb,
+                num_planes: 1,
+            };
+
+            // TODO what happened to re-ordering RTP packets?
             loop {
                 let time = Instant::now();
                 let (amt, src) = sock.recv_from(&mut buf)?;
@@ -41,7 +58,52 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         match decoder.decode(&buffer) {
                             Ok(yuv) => {
                                 h264_data = yuv;
-                                debug!("h264_data {:?}", h264_data);
+                                if let Some(ref yuv_data) = h264_data {
+                                    // Set up target buffer/data for calls to YUV->RGBA conversion
+                                    if let None = rgba_buffer {
+                                        let strides_yuv = yuv_data.strides_yuv();
+                                        width = strides_yuv.0 as u32;
+                                        height = yuv_data.height() as u32;
+                                        rgba_buffer = Some(vec![0; (4 * width * height) as usize]);
+                                    }
+                                    debug!("data width: {}, height: {}", width, height);
+
+                                    let mut src_sizes = [0usize; 3];
+                                    get_buffers_size(
+                                        width,
+                                        height,
+                                        &src_format,
+                                        None,
+                                        &mut src_sizes,
+                                    )?;
+
+                                    let y = &yuv_data.y()[0..]; //src_sizes[0] + 1];
+                                    let u = &yuv_data.u()[0..]; //src_sizes[1] + 1];
+                                    let v = &yuv_data.v()[0..]; //src_sizes[2] + 1];
+
+                                    debug!(
+                                        "converting image... dest buf size is {}, src_sizes is {:#?}, ysize usize vsize: [{}, {}, {}], strides from class are {:?}",
+                                        rgba_buffer.as_mut().unwrap().len(),
+                                        src_sizes, y.len(), u.len(), v.len(),
+                                        yuv_data.strides_yuv()
+                                    );
+
+                                    convert_image(
+                                        width,
+                                        height,
+                                        &src_format,
+                                        None,
+                                        &[y, u, v],
+                                        &dst_format,
+                                        None,
+                                        &mut [&mut *rgba_buffer.as_mut().unwrap()],
+                                    )?;
+
+                                    std::fs::write("frame", &rgba_buffer.as_mut().unwrap())?;
+
+                                    // Convert YUV to Rgba8Uint so it can be copied to wgpu buffer.
+                                }
+                                // debug!("h264_data {:?}", h264_data);
                             }
                             Err(e) => {
                                 error!("Failed to decode pkt {:?}", e)

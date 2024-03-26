852589f054de270e225d586bbd20429974fe199e
Ramesh <me@ramesh.tv>
2024-03-26 13:48:22 -0400

    Update client with feedback sending

--- client/src/decoder/feedback.rs
+++ client/src/decoder/feedback.rs
@@ -4,6 +4,8 @@ use log::{debug, error};
 use net::feedback_packet::LVFeedbackPacket;
 use parking_lot::Mutex;
 
+const QUANTUM: u16 = 1000;
+
 pub fn start(
     feedback_addr: &str,
 ) -> Result<Arc<Mutex<LVFeedbackPacket>>, Box<dyn std::error::Error>> {
@@ -12,12 +14,21 @@ pub fn start(
     let feedback_timer = timer::Timer::new();
     let mut feedback_stream = TcpStream::connect(feedback_addr)?;
 
-    feedback_timer.schedule_repeating(chrono::Duration::seconds(1), move || {
-        let pkt = feedback_packet.lock();
+    feedback_timer.schedule_repeating(chrono::Duration::milliseconds(QUANTUM.into()), move || {
+        debug!("writing feedback packet to server");
+        let mut pkt = feedback_packet.lock();
         match feedback_stream.write(bytemuck::bytes_of(&*pkt)) {
             Ok(bytes) => debug!("wrote {} bytes to feedback server", bytes),
             Err(e) => error!("failed to send feedbacket packet with error {:?}", e),
         }
+
+        // reset the feedback packet
+        pkt.time_quantum = QUANTUM;
+        pkt.total_blocks = 0;
+        pkt.out_of_order_blocks = 0;
+        pkt.total_packets = 0;
+        pkt.lost_packets = 0;
+        pkt.ecc_decoder_failures = 0;
     });
 
     Ok(feedback_packet_clone)
--- client/src/decoder/video.rs
+++ client/src/decoder/video.rs
@@ -241,6 +241,13 @@ impl LVDecoder {
         let mut rs_oorder_packets = 0;
         let mut block_id = 0;
 
+        // TODO offload the feedback to statistics module
+        let mut total_blocks = 0;
+        let mut out_of_order_blocks = 0;
+        let mut total_packets = 0;
+        let mut lost_packets = 0;
+        let mut ecc_decoder_failures = 0;
+
         // TODO what happened to re-ordering RTP packets?
         loop {
             // TODO don't copy. We slice the buffer so it only uses the part of the buffer that was written to by the socket receive.
@@ -266,8 +273,11 @@ impl LVDecoder {
             let lvheader = LVErasureInformation::from_bytes(&data_ext.payload[0..data_ext.amt]);
             let mut rtp_data = &data_ext.payload[LVErasureInformation::no_bytes()..data_ext.amt];
 
+            total_packets += 1;
+
             // new block
             if lvheader.block_id != block_id {
+                total_blocks += 1;
                 // recovery
                 if rs_total_packets < EC_RATIO_RECOVERY_PACKETS + EC_RATIO_REGULAR_PACKETS
                     && rs_total_packets - rs_recovery_packets != EC_RATIO_REGULAR_PACKETS
@@ -309,6 +319,7 @@ impl LVDecoder {
                             }
                         }
                         Err(e) => {
+                            ecc_decoder_failures += 1;
                             warn!("recovery failed with {:?}", e);
                         }
                     }
@@ -366,6 +377,9 @@ impl LVDecoder {
 
             if rs_oorder_packets > 0 {
                 debug!("adding packet to oorder packets");
+
+                lost_packets += lvheader.fragment_index
+                    - ((lvheader_prev_fragment_index + 1) % EC_RATIO_REGULAR_PACKETS);
                 lvheader_prev_fragment_index = lvheader.fragment_index as u32;
                 rs_sendq[lvheader.fragment_index as usize] = packet;
                 rs_oorder_packets += 1;
@@ -379,7 +393,11 @@ impl LVDecoder {
                     "packet out of order: current {} prev {}",
                     lvheader.fragment_index, lvheader_prev_fragment_index
                 );
+                out_of_order_blocks += 1;
+                lost_packets += lvheader.fragment_index
+                    - ((lvheader_prev_fragment_index + 1) % EC_RATIO_REGULAR_PACKETS);
 
+                // TODO this statistic is wrong
                 LVStatisticsCollector::update_data(
                     "client_packets_out_of_order",
                     LVDataPoint::Increment,
@@ -399,6 +417,28 @@ impl LVDecoder {
 
             lvheader_prev_fragment_index = lvheader.fragment_index as u32;
             video_dec.depacketize_decode(&packet)?;
+
+            match feedback_pkt.try_lock() {
+                Some(mut pkt) => {
+                    // Reset our variables
+                    if pkt.total_packets == 0 {
+                        total_blocks = 0;
+                        out_of_order_blocks = 0;
+                        total_packets = 0;
+                        lost_packets = 0;
+                        ecc_decoder_failures = 0;
+                    }
+
+                    pkt.total_blocks = total_blocks;
+                    pkt.out_of_order_blocks = out_of_order_blocks;
+                    pkt.total_packets = total_packets;
+                    pkt.lost_packets = lost_packets as u16;
+                    pkt.ecc_decoder_failures = ecc_decoder_failures;
+                }
+                None => {
+                    warn!("Failed to lock feedback packet")
+                }
+            }
         }
     }
 }
